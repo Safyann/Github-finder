3a76187cd87bab91d24348deaeaf5d593c4d31ab
Alena Safyannikova <alena_sayanniki@mail.ru>
2020-02-07 18:17:08 +0500

    add api and handler errors

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,28 +2,72 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
+        <!-- errors -->
+        <div class="error" v-if="error" style="margin-bottom: 20px">
+          <p>{{ error }}</p>
+        </div>
+
         <!-- search -->
-        <search :value="search" placeholder="Type username..." @search="search = $event" />
-        <button class="btn btnPrimary" @click="getRepos">Search!</button>
+        <search
+          :value="search"
+          placeholder="Type username..."
+          @search="search = $event"
+        />
+
+        <!-- buttons -->
+        <button v-if="!repos" class="btn btnPrimary" @click="getRepos">
+          Search!
+        </button>
+
+        <button v-else class="btn btnPrimary" @click="getRepos">
+          Search again!
+        </button>
+
+        <!-- wrapper -->
+        <div class="repos__wrapper" v-if="repos">
+          <!-- item -->
+          <div class="repo-item" v-for="repo in repos" :key="repo.id">
+            <div class="repos-info">
+              <a class="link" target="_blank" :href="repo.html_url">
+                {{ repo.name }}</a
+              >
+              <span> {{ repo.stargazers_count }}⭐</span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import search from "../components/Search";
+
 export default {
   components: {
     search
   },
   data() {
     return {
-      search: ""
+      search: "",
+      error: null,
+      repos: null
     };
   },
   methods: {
     getRepos() {
-      console.log(`get user ${this.search} repos`);
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+        .then(res => {
+          this.error = null;
+          this.repos = res.data;
+        })
+        .catch(err => {
+          console.log(err);
+          this.repos = null;
+          this.error = "Can`t find this user";
+        });
     }
   }
 };
@@ -38,5 +82,19 @@ export default {
 
 button {
   margin-top: 40px;
+}
+
+.repos__wrapper {
+  width: 400px;
+  margin: 30px 0;
+}
+
+.repos-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px 0;
+  border-bottom: 1px solid #dbdbdb;
 }
 </style>
